48e14cfc26457f439c567819280ff12887514e9d
MMwenda <maur1cemmwenda@gmail.com>
2025-02-05 11:35:08 +0100

    Updates

--- ALGO1/algo.js
+++ ALGO1/algo.js
@@ -17,20 +17,24 @@ function analyzeSentence(sentence) {
         }  
 
     
-        if (char === ' ' && i > 0 && sentence[i - 1] !== ' ') {  
+        if (char === ' ' && i > 0 && sentence[i - 1] !== ' ') {   // if the character is a space and the previous character is not a space then it is a word
             wordCounter++;  
         }  
     }  
 
-    if (lengthCounter > 0 && sentence[lengthCounter - 1] === '.') {  
+    if (lengthCounter > 0 && sentence[lengthCounter - 1] === '.') {  // if the last character is a period then it is a word
         wordCounter++; 
     }  
 
      
     return {  
-        length: lengthCounter - 1,
+        length: lengthCounter,
         words: wordCounter,  
         vowels: vowelCounter  
-    };  
+    };  //return the length of the sentence, the number of words, and the number of vowels as an object
+
+    
 }  
+
+console.log(analyzeSentence('Mock sentence of you reading a sentence.'));  // { length: 40, words: 7, vowels: 14}
 
